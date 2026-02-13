13d27e3cf1da9281b163a99859ffcd50c0420239
seon23097-source <seon23097@gmail.com>
2026-02-13 21:30:47 +0900

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend/',  // GitHub Pages용 (repository 이름)
+  base: '/',  // GitHub Pages용 (repository 이름)
   server: {
     port: 3000,
     proxy: {
